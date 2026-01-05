58c7e5bc22509f82a33751dd9b47c11849187bfa
BoneDeath <mhasannudin300@gmail.com>
2026-01-05 11:30:04 +0700

    change to cookies

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import routes from "./routes";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api", routes);
 app.get("/", (req, res) => {
   res.json({ message: "API is running" });
